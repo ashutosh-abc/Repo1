972595ca9280f27c1aaa65eb1dc6546aeb2187bb
ashutosh-abc <ashutoshnemani3@gmail.com>
2018-08-24 10:10:36 +0530

    updated file

---
+++ MyClass.java
@@ -0,0 +1,20 @@
+public class MyClass
+{
+  static void main(String[]abcd)
+   {
+     System.out.println("My Class");
+   }
+
+
+
+
+}
+class YourClass
+{
+ public static void main(String [] args)
+   {
+     System.out.println("Your Class");
+   }
+
+
+}
